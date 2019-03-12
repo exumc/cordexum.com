262426cba49fbe3c27c0f6ed3d1cbf19fe047358
exumc <cordexum@gmail.com>
2019-03-11 18:26:25 -0700

    update build

---
+++ build/js/init.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  M.AutoInit();
+  $('.sidenav').sidenav();
+  $("#webdev").show();
+  $("#gdesign").toggle();
+  $("#photography").toggle();
+
+
+  $("#webdev-btn").click(function () {
+    $("#webdev").fadeIn("slow");
+    $("#gdesign").fadeOut();
+    $("#photography").fadeOut();
+
+  });
+
+  $("#gdesign-btn").click(function () {
+    $("#gdesign").fadeIn("slow");
+    $("#webdev").fadeOut();
+    $("#photography").fadeOut();
+  });
+
+  $("#photography-btn").click(function () {
+    $("#photography").fadeIn("slow");
+    $("#gdesign").fadeOut();
+    $("#webdev").fadeOut();
+
+  });
+
+  $(window).scroll(function () {
+    $(".slideanim").each(function () {
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+});
+
