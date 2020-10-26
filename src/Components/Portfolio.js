3cd518e288170beeadb867c53b461cfe2a67749e
exumc <cordexum@gmail.com>
2020-10-26 15:30:25 -0700

    fix

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends React.Component {
   render() {
     return (
       <div className="mainPage">
-        <div className="container portfolio-links">
+        {/* <div className="container portfolio-links">
           <div>
             <Link to="/home">Studio</Link>
           </div>
@@ -22,7 +22,7 @@ class Portfolio extends React.Component {
 
         <Route path="/home" component={Studio} />
         <Route path="/home/events" component={Events} />
-        <Route path="/home/portraits" component={Portraits} />
+        <Route path="/home/portraits" component={Portraits} /> */}
       </div>
     );
   }
