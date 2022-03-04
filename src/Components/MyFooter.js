671aff42dfdcb0db43cbdbc0979d24c306e9f85f
exumc <cordexum@gmail.com>
2022-03-04 15:45:04 -0800

    added current year function to footer

--- src/Components/MyFooter.js
+++ src/Components/MyFooter.js
@@ -1,11 +1,23 @@
 import React from "react";
+import dayjs from "dayjs";
+
+const currentYear = () => {
+  let now = dayjs();
+  let year = now.format("YYYY");
+  document.getElementById("year").innerHTML = year;
+};
 
 class MyFooter extends React.Component {
+  componentDidMount() {
+    currentYear();
+  }
   render() {
     return (
       <div>
         <div className="container center-align">
-          <p className="light">© 2020 Cord Exum. All rights reserved.</p>
+          <p className="light">
+            © 2020 - <span id="year" /> Cord Exum. All rights reserved.
+          </p>
         </div>
       </div>
     );
